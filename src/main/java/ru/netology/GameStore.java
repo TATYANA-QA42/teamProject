94cd06d8fe914c36074ba9160307bad20016ea0f
Татьяна Пислегина <ztv-1988@yandex.ru>
2022-09-27 10:15:53 +0500

    gamestoretest

--- src/main/java/ru/netology/GameStore.java
+++ src/main/java/ru/netology/GameStore.java
@@ -19,7 +19,7 @@ public class GameStore {
      * Создание объекта игры с заданными заголовком и жанром
      * Каждый объект игры помнит объект каталога, которому она принадлежит
      */
-    public Game publishGame(String title, String genre) { // проверить
+    public Game publishGame(String title, String genre) {
         Game game = new Game(title, genre, this);
         games.add(game);
         return game;
@@ -29,8 +29,7 @@ public class GameStore {
      * Проверяет наличие игры в каталоге и возврашает true
      * если игра есть и false иначе
      */
-    public boolean containsGame(Game game) {// исправлено. но проверить
-        games.add(game); //дефект;
+    public boolean containsGame(Game game) {
         for (int i = 1; i < games.size(); i++) {
             if (games.get(i - 1).equals(game)) {
                 return true;
@@ -44,9 +43,9 @@ public class GameStore {
      * за игрой этого каталога. Игрок задаётся по имени. Время должно
      * суммироваться с прошлым значением для этого игрока
      */
-    public void addPlayTime(String playerName, int hours) {//так же как и в игроке цикл и проверки условий
+    public void addPlayTime(String playerName, int hours) {
         if (playedTime.containsKey(playerName)) {
-            playedTime.put(playerName, playedTime.get(playerName) + hours);
+            playedTime.put(playerName, playedTime.get(playerName));
         } else {
             playedTime.put(playerName, hours);
         }
@@ -56,15 +55,13 @@ public class GameStore {
      * Ищет имя игрока, который играл в игры этого каталога больше всего
      * времени. Если игроков нет, то возвращется null
      */
-    public String getMostPlayer() { // проверка и доп условия по анализу игроков
+    public String getMostPlayer() {
         int mostTime = 1;
         String bestPlayer = null;
         for (String playerName : playedTime.keySet()) {
             int playerTime = playedTime.get(playerName);
-            if (playerTime >= mostTime) {
+            if (playerTime > mostTime) {
                 mostTime = playerTime;
-//                String[] bestPlayer = new String[];
-//                for(int i=0, i<bestPlayer.length, i++){
                 bestPlayer = playerName;
             }
         }
@@ -76,10 +73,91 @@ public class GameStore {
      * за играми этого каталога
      */
     public int getSumPlayedTime() {
-        int sum = 0;
-        for (int value : playedTime.values()) {
-            sum += value;
-        }
-        return sum;
+        return 0;
     }
-}//полностью реализоват метод подсчета времени всех игроков
+}
+//package ru.netology;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//public class GameStore {
+//    private List<Game> games = new ArrayList<>();
+//
+//    /**
+//     * Информация о том, какой игрок сколько играл в игры этого каталога
+//     * Ключ - имя игрока
+//     * Значение - суммарное количество часов в игры этого каталога
+//     */
+//    private Map<String, Integer> playedTime = new HashMap<>();
+//
+//    /**
+//     * Создание объекта игры с заданными заголовком и жанром
+//     * Каждый объект игры помнит объект каталога, которому она принадлежит
+//     */
+//    public Game publishGame(String title, String genre) { // проверить
+//        Game game = new Game(title, genre, this);
+//        games.add(game);
+//        return game;
+//    }
+//
+//    /**
+//     * Проверяет наличие игры в каталоге и возврашает true
+//     * если игра есть и false иначе
+//     */
+//    public boolean containsGame(Game game) {// исправлено. но проверить
+//        games.add(game); //дефект;
+//        for (int i = 1; i < games.size(); i++) {
+//            if (games.get(i - 1).equals(game)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    /**
+//     * Регистрирует количество времени, которое проиграл игрок
+//     * за игрой этого каталога. Игрок задаётся по имени. Время должно
+//     * суммироваться с прошлым значением для этого игрока
+//     */
+//    public void addPlayTime(String playerName, int hours) {//так же как и в игроке цикл и проверки условий
+//        if (playedTime.containsKey(playerName)) {
+//            playedTime.put(playerName, playedTime.get(playerName) + hours);
+//        } else {
+//            playedTime.put(playerName, hours);
+//        }
+//    }
+//
+//    /**
+//     * Ищет имя игрока, который играл в игры этого каталога больше всего
+//     * времени. Если игроков нет, то возвращется null
+//     */
+//    public String getMostPlayer() { // проверка и доп условия по анализу игроков
+//        int mostTime = 1;
+//        String bestPlayer = null;
+//        for (String playerName : playedTime.keySet()) {
+//            int playerTime = playedTime.get(playerName);
+//            if (playerTime >= mostTime) {
+//                mostTime = playerTime;
+////                String[] bestPlayer = new String[];
+////                for(int i=0, i<bestPlayer.length, i++){
+//                bestPlayer = playerName;
+//            }
+//        }
+//        return bestPlayer;
+//    }
+//
+//    /**
+//     * Суммирует общее количество времени всех игроков, проведённого
+//     * за играми этого каталога
+//     */
+//    public int getSumPlayedTime() {
+//        int sum = 0;
+//        for (int value : playedTime.values()) {
+//            sum += value;
+//        }
+//        return sum;
+//    }
+//}//полностью реализоват метод подсчета времени всех игроков
